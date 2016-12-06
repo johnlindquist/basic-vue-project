bcb2b5bf0ea71511db02536828f2b50edab99422
John Lindquist <johnlindquist@gmail.com>
2016-12-06 11:36:39 -0700

    pre-loading data with promises

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <h1>{{message}}</h1>
-    <input v-model="message">
-
+    <ul>
+      <li v-for="person in people">{{person.name}}</li>
+    </ul>
   </div>
 </template>
 <script>
+import axios from 'axios'
+
+const api = `https://swapi-json-server-nvaxelgbew.now.sh/people`
+
 export default {
-  data(){return{
-    message: `My awesome message`
-  }}
+  data(){return axios.get(api)
+    .then(res => ({people:res.data}))
+  }
 }
 </script>
